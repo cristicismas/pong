c615f5257db261f91d5c0f764bda3a30c1cafcd1
Cristi Cismas <cristicismas99@gmail.com>
2020-09-15 17:56:16 +0000

    Add logic for drawing and moving the paddles

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+mod systems;
+
+mod pong;
+use crate::pong::Pong;
+
 use amethyst::{
+    input::{InputBundle, StringBindings},
+    core::transform::TransformBundle,
     prelude::*,
     renderer::{
         plugins::{RenderFlat2D, RenderToWindow},
@@ -8,24 +15,25 @@ use amethyst::{
     utils::application_root_dir,
 };
 
-pub struct Pong;
-
-impl SimpleState for Pong {
-
-}
-
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
 
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
+    let binding_path = app_root.join("config").join("bindings.ron");
+
+    let input_bundle =
+        InputBundle::<StringBindings>::new().with_bindings_from_file(binding_path)?;
 
     let game_data = GameDataBuilder::default()
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(input_bundle)?
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"])
         .with_bundle(
             RenderingBundle::<DefaultBackend>::new()
                 .with_plugin(
                     RenderToWindow::from_config_path(display_config_path)?
-                        .with_clear([0.00196, 0.23726, 0.21765, 1.0]),
+                        .with_clear([0.0, 0.0, 0.0, 1.0]),
                 )
                 .with_plugin(RenderFlat2D::default()),
         )?;
